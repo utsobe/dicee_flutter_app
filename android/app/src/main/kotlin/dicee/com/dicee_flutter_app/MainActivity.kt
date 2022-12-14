29a8fa067ff2c762ba80f1f33b83e5941df5536e
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-12-14 17:56:10 +0800

    initialize flutter app

---
+++ android/app/src/main/kotlin/dicee/com/dicee_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dicee.com.dicee_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
